7e8dfb455df80c44e342e8cefc9f1692e56ab115
joaobessada <joaobessada@gmail.com>
2022-11-29 15:07:36 -0300

    created

---
+++ src/main/java/com/bessada/webservicesspring/WebservicesspringApplication.java
@@ -0,0 +1,13 @@
+package com.bessada.webservicesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bessada/webservicesspring/WebservicesspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bessada.webservicesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
