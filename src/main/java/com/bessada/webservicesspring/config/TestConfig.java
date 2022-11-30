eb00e3952fb26c2f0fc9f6dc46b1934819b7204a
joaobessada <joaobessada@gmail.com>
2022-11-30 12:27:29 -0300

    Payment, one-to-one association

--- src/main/java/com/bessada/webservicesspring/config/TestConfig.java
+++ src/main/java/com/bessada/webservicesspring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bessada.webservicesspring.entities.Category;
 import com.bessada.webservicesspring.entities.Order;
 import com.bessada.webservicesspring.entities.OrderItem;
+import com.bessada.webservicesspring.entities.Payment;
 import com.bessada.webservicesspring.entities.Product;
 import com.bessada.webservicesspring.entities.User;
 import com.bessada.webservicesspring.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
